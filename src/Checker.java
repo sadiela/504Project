57ef8261b3e485786266aeed5c9b42838a99e79f
usef-kh <yousif.khaireddin@gmail.com>
2020-02-27 16:01:26 -0500

    Fixed bug in checker

--- src/Checker.java
+++ src/Checker.java
@@ -11,17 +11,28 @@ public class Checker {
         relationships = relat;
     }
 
-    int suspicionCalculator(String s)
-    {
-        int suspicionCounter = 0;
+    double check(String s) {
+
+        double suspicionCount = 0;
         String[] words = s.split(" ");
-        for(int i = 0; i < words.length-1; i++)
+
+        for (int i = 0; i < words.length - 1; i++)
         {
-            if(relationships[allWords.getOrDefault(words[i], 0)][allWords.getOrDefault(words[i + 1], 0)])
-            {
-                suspicionCounter++;
+            Integer id1 = allWords.getOrDefault(words[i], null);
+            Integer id2 = allWords.getOrDefault(words[i + 1], null);
+
+            // check that the first word exists and that the second word has followed it before
+            if (id1 == null || (id2 != null && !relationships[id1][id2])) {
+                suspicionCount++;
             }
         }
-        return 1 - suspicionCounter/(words.length-1);
+
+        // verify that the last word is in the hashmap
+        if (!allWords.containsKey(words[words.length - 1]))
+        {
+            suspicionCount++;
+        }
+
+        return suspicionCount/words.length;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,24 @@ public class Main {
     public static void main(String[] args)
     {
         Vector<String> a = new Vector<>();
-        a.add("This is our first sentence");
-        a.add( "Here is another sentence");
+        a.add("this is our first sentence");
+        a.add( "here is another sentence");
         HashMap<String,Integer> hm = new HashMap<>();
-        hm.put("This", 0);
+        hm.put("this", 0);
         hm.put("is", 1);
         hm.put("our", 2);
         hm.put("first", 3);
         hm.put("sentence", 4);
-        hm.put("Here", 5);
+        hm.put("here", 5);
         hm.put("another", 6);
 
         DB myDB = new DB(hm, a);
         System.out.println("Done");
 
         Checker myCheck = new Checker(hm, myDB.relationships);
-        System.out.println(myCheck.suspicionCalculator("is this suspicious"));
-        System.out.println(myCheck.suspicionCalculator("our first sentence"));
+        System.out.println(myCheck.check("this is our first sentence"));
+        System.out.println(myCheck.check("is this suspicious"));
+        System.out.println(myCheck.check("i bought a pineapple"));
     }
 
 }
