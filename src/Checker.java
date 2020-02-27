acb7baa34f595a6cb001dc40a8c6c8227c7d9543
usef-kh <yousif.khaireddin@gmail.com>
2020-02-27 16:07:13 -0500

    Changed variable names

--- src/Checker.java
+++ src/Checker.java
@@ -2,24 +2,24 @@ import java.util.HashMap;
 import java.util.Vector;
 
 public class Checker {
-    HashMap<String, Integer> allWords;
+    HashMap<String, Integer> wordMap;
     boolean[][] relationships;
 
-    public Checker(HashMap<String, Integer> w, boolean[][] relat)
+    public Checker(HashMap<String, Integer> wm, boolean[][] relat)
     {
-        allWords = w;
+        wordMap = wm;
         relationships = relat;
     }
 
-    double check(String s) {
+    double check(String phrase) {
 
         double suspicionCount = 0;
-        String[] words = s.split(" ");
+        String[] words = phrase.split(" ");
 
         for (int i = 0; i < words.length - 1; i++)
         {
-            Integer id1 = allWords.getOrDefault(words[i], null);
-            Integer id2 = allWords.getOrDefault(words[i + 1], null);
+            Integer id1 = wordMap.getOrDefault(words[i], null);
+            Integer id2 = wordMap.getOrDefault(words[i + 1], null);
 
             // check that the first word exists and that the second word has followed it before
             if (id1 == null || (id2 != null && !relationships[id1][id2])) {
@@ -28,7 +28,7 @@ public class Checker {
         }
 
         // verify that the last word is in the hashmap
-        if (!allWords.containsKey(words[words.length - 1]))
+        if (!wordMap.containsKey(words[words.length - 1]))
         {
             suspicionCount++;
         }
--- src/DB.java
+++ src/DB.java
@@ -3,29 +3,29 @@ import java.util.Vector;
 
 public class DB {
     // fields
-    HashMap<String, Integer> allWords;
+    HashMap<String, Integer> wordMap;
     boolean[][] relationships;
 
     public DB(HashMap<String, Integer> w, Vector<String> sentences)
     {
-        allWords = w;
+        wordMap = w;
         relationships = new boolean[w.size()][w.size()];
         for(String s:sentences)
         {
             String[] words = s.split(" ");
             for(int i = 0; i < words.length-1; i++)
             {
-                relationships[allWords.get(words[i])][allWords.get(words[i+1])] = true;
+                relationships[wordMap.get(words[i])][wordMap.get(words[i+1])] = true;
             }
         }
     }
 
     // save to file... decide format
 
-    public void addSentence(String s)
+    public void addSentence(String sentence)
     {
         // split into individual words
-        String[] words = s.split(" ");
+        String[] words = sentence.split(" ");
         for(String w:words)
         {
 
